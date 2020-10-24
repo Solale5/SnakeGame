a3790cd728535e1c2a61bc8f2ad8240e9b50962c
Solomon Alemu <{ID}+{username}@users.noreply.github.com>
2020-10-23 17:42:04 -0700

    Initial commit

---
+++ GameFrame.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class GameFrame extends JFrame {
+    public GameFrame() {
+        this.add(new GamePanel());
+        this.setTitle("Snake");
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setResizable(false);
+
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+
+    }
+
+
+}
